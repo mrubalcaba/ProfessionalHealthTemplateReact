73fd877cea1280a9a764b4a71893037cab628e95
Melanie <melanienmedina@gmail.com>
2021-03-25 20:00:15 -0700

    creating subheading for home page

---
+++ healthbusinessapp/src/components/Subheading.js
@@ -0,0 +1,25 @@
+
+
+const Subheading = () => {
+    return (
+        <section className="subheading">
+           <div className="container">
+                <div className="left-col">
+                    <img src="./../images/women-in-nature.jpg" alt="Women in nature"/>
+                </div>
+                <div className="right-col">
+                    <h1 className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Phasellus hendrerit diam in scelerisque efficitur. </h1>
+                    <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Phasellus hendrerit diam in scelerisque efficitur. Class 
+                        aptent taciti sociosqu ad litora torquent per conubia 
+                        nostra, per inceptos himenaeos. Fusce in elit non magna 
+                        congue varius id eget erat. </h4>
+                    <button>Learn More</button>
+                </div>
+            </div> 
+        </section>
+    )
+}
+
+export default Subheading
