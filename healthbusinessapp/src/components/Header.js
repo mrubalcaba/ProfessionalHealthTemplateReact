71271b18f8f79dbaaa75fdf4e5b304e8e011ffff
Melanie <melanienmedina@gmail.com>
2021-03-25 18:09:57 -0700

    adding header components

--- healthbusinessapp/src/components/Header.js
+++ healthbusinessapp/src/components/Header.js
@@ -1,6 +1,31 @@
+
+
 const Header = () => {
     return (
-        <header>Almar</header>
+        <div className="navbar">
+            <div className="container">
+                {/* want the header to reference the home page */}
+                <a href="/">Almar</a>
+                {/* <header>Almar</header> */}
+                <img id="mobile-cta" className="mobile-menu" src="./../images/menu.svg" alt="Open Navigation"/>
+                <nav>
+                    <img id="mobile-exit" className="mobile-menu-exit" src="./../images/cancel.svg" alt="Close Navigation"/>
+                    <ul className="primary-nav">
+                        <li className="current"><a href="/">You</a></li>
+                        <li><a href="/">Blog</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Contact</a></li>
+                        
+                    </ul>
+                    <ul className="secondary-nav">
+                        <li><a href="/">Login</a></li>
+                        <li className="get-started-cta"><a href="/">Get Started</a></li>
+                        
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        
     )
 }
 
