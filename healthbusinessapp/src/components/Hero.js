b92bcfda81d5bd07746c03e59e474e491f836958
Melanie <melanienmedina@gmail.com>
2021-03-26 00:05:31 -0700

    update to hero content

--- healthbusinessapp/src/components/Hero.js
+++ healthbusinessapp/src/components/Hero.js
@@ -3,7 +3,9 @@ const Hero = () => {
         <section className="hero">
            <div className="container">
                 <h1>Find your happiness and learn to put it first.</h1>
-                <a href="/">Chat with me</a>
+                {/* will reference the same as get started button in header */}
+                <p><a href="/">Chat with me</a></p>
+                
             </div> 
         </section>
     )
