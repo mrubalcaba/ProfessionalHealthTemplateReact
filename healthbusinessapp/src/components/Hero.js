eb1ce7cc1201d93f3015b3ff9b07a2ed40a9dcd2
Melanie <melanienmedina@gmail.com>
2021-03-25 19:59:30 -0700

    creating hero section

---
+++ healthbusinessapp/src/components/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+    return (
+        <section className="hero">
+           <div className="container">
+                <h1>Find your happiness and learn to put it first.</h1>
+                <a href="/">Chat with me</a>
+            </div> 
+        </section>
+    )
+}
+
+export default Hero
