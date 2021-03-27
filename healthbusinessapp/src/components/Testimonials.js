26ce6a6571fb66a98ee24bcd3b668a7e9c7d35c4
Melanie <melanienmedina@gmail.com>
2021-03-27 00:41:56 -0700

    adding testimonials section

---
+++ healthbusinessapp/src/components/Testimonials.js
@@ -0,0 +1,36 @@
+const Testimonials = () => {
+    return (
+      <section className="testimonials-section">
+          <div className="container">
+              <ul>
+                  <li>
+                      <img src="./../images/person.jpg" alt="Person"/>
+                      <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Phasellus hendrerit diam in scelerisque efficitur. Class 
+                        aptent taciti sociosqu ad litora torquent per conubia 
+                        nostra, per inceptos himenaeos."</blockquote>
+                        <cite>- Jane Doe</cite>
+                  </li>
+                  <li>
+                      <img src="./../images/person.jpg" alt="Person"/>
+                      <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Phasellus hendrerit diam in scelerisque efficitur. Class 
+                        aptent taciti sociosqu ad litora torquent per conubia 
+                        nostra, per inceptos himenaeos."</blockquote>
+                        <cite>- Jane Doe</cite>
+                  </li>
+                  <li>
+                      <img src="./../images/person.jpg" alt="Person"/>
+                      <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Phasellus hendrerit diam in scelerisque efficitur. Class 
+                        aptent taciti sociosqu ad litora torquent per conubia 
+                        nostra, per inceptos himenaeos."</blockquote>
+                        <cite>- Jane Doe</cite>
+                  </li>
+              </ul>
+          </div>
+      </section>
+    )
+}
+
+export default Testimonials
