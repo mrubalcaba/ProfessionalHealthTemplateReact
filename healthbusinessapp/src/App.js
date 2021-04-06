994dc26857b5d6f1d17a9d74ccd1d53e3923a143
Melanie <melanienmedina@gmail.com>
2021-04-05 21:53:07 -0700

    implemented hero with header

--- healthbusinessapp/src/App.js
+++ healthbusinessapp/src/App.js
@@ -1,6 +1,5 @@
 import './css/main.scss';
 import Header from './components/Header'
-import Hero from './components/Hero'
 import Features from './components/Features';
 import Testimonials from './components/Testimonials';
 import Newsletter from './components/Newsletter';
@@ -10,7 +9,6 @@ function App() {
   return (
     <div>
         <Header></Header>
-        <Hero></Hero>
         <Features></Features>
         <Testimonials></Testimonials>
         <Newsletter></Newsletter>
