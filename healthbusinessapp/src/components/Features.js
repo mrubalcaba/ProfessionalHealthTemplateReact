03e67f6097f7ea51e05b050169f941db4ac01c6d
Melanie <melanienmedina@gmail.com>
2021-04-10 00:12:43 -0700

    features section 1.0

--- healthbusinessapp/src/components/Features.js
+++ healthbusinessapp/src/components/Features.js
@@ -4,32 +4,31 @@ const Features = () => {
     return (
         <section className="features">
            <div className="container">
-               <div className="features-img">
-                <img className="nature-img" src="./../images/women-in-nature.jpg" alt="Women in nature"/>
+               <div className="marketing-img">
+                    <img className="feature-img" src="./../images/women-in-nature.jpg" alt="Women in nature"/>
                </div>
-                <div className="right-col">
-                    <h1 className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Phasellus hendrerit diam in scelerisque efficitur. </h1>
+                <div className="right-col col">
+                    <h1 className="self-care-msg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
                     <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                         Phasellus hendrerit diam in scelerisque efficitur. Class 
                         aptent taciti sociosqu ad litora torquent per conubia 
                         nostra, per inceptos himenaeos. Fusce in elit non magna 
                         congue varius id eget erat. </h4>
-                    <button>Learn More</button>
+                    <button className="learn-more">Learn More</button>
                 </div>
             </div> 
             <div className="container">
-                <div className="left-col">
-                    <h1 className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Phasellus hendrerit diam in scelerisque efficitur. </h1>
+                <div className="next-features-img">
+                    <img className="feature-img" src="./../images/group-laughing.jpg" alt="Group laughing"/>
+                </div>
+                <div className="left-col col">
+                    <h1 className="self-care-msg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
                     <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                         Phasellus hendrerit diam in scelerisque efficitur. Class 
                         aptent taciti sociosqu ad litora torquent per conubia 
-                        nostra, per inceptos himenaeos. Fusce in elit non magna 
-                        congue varius id eget erat. </h4>
-                    <button>Learn More</button>
+                        nostra, per inceptos himenaeos.</h4>
+                    <button className="learn-more friend-learn">Learn More</button>
                 </div>
-                <img className="features-image" src="./../images/group-laughing.jpg" alt="Group laughing"/>
             </div> 
         </section>
     )
