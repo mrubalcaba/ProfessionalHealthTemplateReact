a714f582fe28915de9e2ceed364f43f5b11973b8
Melanie <melanienmedina@gmail.com>
2021-04-12 22:18:55 -0700

    added toggable nav

--- healthbusinessapp/src/components/Header.js
+++ healthbusinessapp/src/components/Header.js
@@ -1,21 +1,26 @@
-
+import { useState } from "react";
 
 const Header = () => {
+    const [showNav, toggleNav] = useState(false);
+    const toggle = () => {
+        toggleNav(!showNav);
+    }
+
     return (
         <div className="navbar">
             <div className="nav-background-img">
                 <img className="person-background-img" src="./../images/light-picture.jpg" alt="Person"/>
                 <div className="navbar-top">
                     <div className="logo" href="/">Almar</div>
-                    <img id="mobile-cta" className="mobile-menu" src="./../images/menu.svg" alt="Open Navigation"/>
+                    <img id="mobile-cta" className="mobile-menu" onClick={toggle} src="./../images/menu.svg" alt="Open Navigation"/>
                 </div>
                 <section className="hero">
                     <h1>Find your happiness and learn to put it first.</h1>
                     {/* will reference the same as get started button in header */}
                     <p><div className="membership-redirect">Chat with me</div></p>
                 </section>  
-                <nav className="desktop-nav">
-                    <img id="mobile-exit" className="mobile-menu-exit" src="./../images/cancel.svg" alt="Close Navigation"/>
+                {showNav ? <nav className="desktop-nav">
+                    <img id="mobile-exit" className="mobile-menu-exit" onClick={toggle} src="./../images/cancel.svg" alt="Close Navigation"/>
                     <ul className="primary-nav">
                         <li className="current"><a href="/">You</a></li>
                         <li><a href="/">Blog</a></li>
@@ -28,7 +33,7 @@ const Header = () => {
                         <li className="get-started-cta"><a href="/">Get Started</a></li>
                         
                     </ul>
-                </nav>
+                </nav>: null}
             </div>
         </div>
         
