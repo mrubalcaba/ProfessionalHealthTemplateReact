c1c3a95f5f5d0b013c42455231308b9cab4d85fc
Melanie <melanienmedina@gmail.com>
2021-04-24 01:20:37 -0700

    youfeature.js 1.0

--- healthbusinessapp/src/components/YouFeature.js
+++ healthbusinessapp/src/components/YouFeature.js
@@ -14,6 +14,28 @@ const YouFeature = () => {
                         </section>
                     </div>
                 </div>
+                <div className="you-ex-features">
+                    <h2>Focusing on You</h2>
+                    <div className="ex-features">
+                        <h3>01 - Example Feature</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dui quam, varius at mi at, faucibus.</p>
+                        <h3>02 - Example Feature</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dui quam, varius at mi at, faucibus.</p>
+                        <h3>03 - Example Feature</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dui quam, varius at mi at, faucibus.</p>
+                        <h3>04 - Example Feature</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dui quam, varius at mi at, faucibus.</p>
+                    </div>
+                </div>
+                <div className="feature-msg">
+                    <div className="feature-img-background">
+                        <img src="./../images/mountains.jpg" alt="person" className="you-feature-img"/>
+                        <section className="you-feature-content">
+                            <h1>If you face your flaws, you’ll notice the change.</h1>
+                            <p>Lorem ipsum dolor sit amet nullam vel ultricies metus.</p>
+                        </section>
+                    </div>
+                </div>
                 <Footer/>
             </Navbar>
         </div>
