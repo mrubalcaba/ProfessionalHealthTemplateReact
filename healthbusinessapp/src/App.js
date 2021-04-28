a09c3dc2925759b42986d3391e3c939cf0afe265
Melanie <melanienmedina@gmail.com>
2021-04-27 19:33:58 -0700

    passing width from home to features

--- healthbusinessapp/src/App.js
+++ healthbusinessapp/src/App.js
@@ -8,52 +8,16 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import YouFeature from './components/YouFeature';
 import { useState, useEffect } from 'react';
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-
-function useWindowSize() {
-	// Initialize state with undefined width/height so server and client renders match
-	// Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-	const [ windowSize, setWindowSize ] = useState({
-		width: undefined,
-		height: undefined
-	});
-	useEffect(() => {
-		// Handler to call on window resize
-		function handleResize() {
-			// Set window width/height to state
-			setWindowSize({
-				width: window.innerWidth,
-				height: window.innerHeight
-			});
-		}
-		// Add event listener
-		window.addEventListener('resize', handleResize);
-		// Call handler right away so state gets updated with initial window size
-		handleResize();
-		// Remove event listener on cleanup
-		return () => window.removeEventListener('resize', handleResize);
-	}, []); // Empty array ensures that effect is only run on mount
-	return windowSize;
-}
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-	const size = useWindowSize();
-
 	return (
 		<Router>
 			<div>
 				<Navbar>
-					<Route
-						exact={true}
-						path="/"
-						component={Home}
-					/>
-					<Route
-						exact={true}
-						path="/YouFeature"
-						component={YouFeature}
-					/>
+					<Route exact={true} path="/" component={Home} />
+					<Route exact={true} path="/YouFeature" component={YouFeature} />
 				</Navbar>
 			</div>
 		</Router>
--- healthbusinessapp/src/components/Home.js
+++ healthbusinessapp/src/components/Home.js
@@ -3,12 +3,40 @@ import Features from './Features';
 import Testimonials from './Testimonials';
 import Newsletter from './Newsletter';
 import Footer from './Footer';
+import { useState, useEffect } from 'react';
+
+function useWindowSize() {
+	// Initialize state with undefined width/height so server and client renders match
+	// Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+	const [ windowSize, setWindowSize ] = useState({
+		width: undefined,
+		height: undefined
+	});
+	useEffect(() => {
+		// Handler to call on window resize
+		function handleResize() {
+			// Set window width/height to state
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight
+			});
+		}
+		// Add event listener
+		window.addEventListener('resize', handleResize);
+		// Call handler right away so state gets updated with initial window size
+		handleResize();
+		// Remove event listener on cleanup
+		return () => window.removeEventListener('resize', handleResize);
+	}, []); // Empty array ensures that effect is only run on mount
+	return windowSize;
+}
 
 const Home = () => {
+    const size = useWindowSize();
     return (
         <div>
             <Hero/>
-            <Features/>
+            <Features width={size.width}/>
             <Testimonials/>
             <Newsletter/>
             <Footer/>
