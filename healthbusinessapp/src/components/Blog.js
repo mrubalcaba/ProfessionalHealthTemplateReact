cc18b5ba7527b5f36f6a244b98630da5f0e1038a
Melanie <melanienmedina@gmail.com>
2021-05-10 21:45:45 -0700

    blog styling

--- healthbusinessapp/src/components/Blog.js
+++ healthbusinessapp/src/components/Blog.js
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import Footer from './Footer.js';
 
 const Blog = () => {
     return (
@@ -13,7 +14,7 @@ const Blog = () => {
             <div className="blog-basic-grid">
                 <article className="blog-article">
                     <Link to="/">
-                        <img className="blog-img" src="./../images/person-looking-out.jpg" alt="word-breathe-nature"/>
+                        <img className="blog-img" src="./../images/get-started.jpg" alt="word-breathe-nature"/>
                     </Link>
                     <div>3/11/19</div>
                     <h1>On Letting Go</h1>
@@ -28,7 +29,7 @@ const Blog = () => {
                         <img className="blog-img" src="./../images/railroad-walking.jpg" alt="word-breathe-nature"/>
                     </Link>
                     <div>3/11/19</div>
-                    <h1>On Letting Go</h1>
+                    <h1>Toxic Relationships</h1>
                     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque 
                         ipsam sapiente, reprehenderit vel deserunt amet labore blanditiis 
                         sequi quos quasi, corrupti perferendis sed nisi provident aspernatur 
@@ -39,44 +40,56 @@ const Blog = () => {
                     <Link to="/">
                         <img className="blog-img" src="./../images/boundaries.jpg" alt="word-breathe-nature"/>
                     </Link>
-                    <div></div>
-                    <h1></h1>
-                    <div></div>
-                    <a href=""></a>
+                    <div>3/11/19</div>
+                    <h1>Setting Boundaries</h1>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque 
+                        ipsam sapiente, reprehenderit vel deserunt amet labore blanditiis 
+                        sequi quos quasi, corrupti perferendis sed nisi provident aspernatur 
+                    </div>
+                    <a href="">Read More</a>
                 </article>
                 <article className="blog-article">
                     <Link to="/">
                         <img className="blog-img" src="./../images/insp-quote.jpg" alt="word-breathe-nature"/>
                     </Link>
-                    <div></div>
-                    <h1></h1>
-                    <div></div>
-                    <a href=""></a>
+                    <div>3/11/19</div>
+                    <h1>Change for the Better</h1>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque 
+                        ipsam sapiente, reprehenderit vel deserunt amet labore blanditiis 
+                        sequi quos quasi, corrupti perferendis sed nisi provident aspernatur 
+                    </div>
+                    <a href="">Read More</a>
                 </article>
                 <article className="blog-article">
                     <Link to="/">
                         <img className="blog-img" src="./../images/creative.jpg" alt="word-breathe-nature"/>
                     </Link>
-                    <div></div>
-                    <h1></h1>
-                    <div></div>
-                    <a href=""></a>
+                    <div>3/11/19</div>
+                    <h1>It's Okay to Fail</h1>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque 
+                        ipsam sapiente, reprehenderit vel deserunt amet labore blanditiis 
+                        sequi quos quasi, corrupti perferendis sed nisi provident aspernatur 
+                    </div>
+                    <a href="">Read More</a>
                 </article>
                 <article className="blog-article">
                     <Link to="/">
                         <img className="blog-img" src="./../images/say-si.jpg" alt="word-breathe-nature"/>
                     </Link>
-                    <div></div>
-                    <h1></h1>
-                    <div></div>
-                    <a href=""></a>
+                    <div>3/11/19</div>
+                    <h1>Say Yes</h1>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque 
+                        ipsam sapiente, reprehenderit vel deserunt amet labore blanditiis 
+                        sequi quos quasi, corrupti perferendis sed nisi provident aspernatur 
+                    </div>
+                    <a href="">Read More</a>
                 </article>
             </div>
             <div className="blog-section-cta">
                 <div className="blog-content-wrapper">
                     <div className="blog-content-cta">
-                        <h2>Start your journey</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur 
+                        <h2 className="journey-msg">Start your journey</h2>
+                        <p className="journey-msg"> Lorem ipsum dolor sit amet, consectetur 
                             adipiscing elit, sed do eiusmod tempor 
                             incididunt ut labore et dolore magna aliqua.
                         </p>
@@ -86,6 +99,7 @@ const Blog = () => {
                     </div>
                 </div>
             </div>
+            <Footer/>
         </div>
     )
 }
