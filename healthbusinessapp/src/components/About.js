9eb81c732b7197307338000af7e38fc516236aef
Melanie <melanienmedina@gmail.com>
2021-05-14 00:03:09 -0700

    working on about styling

--- healthbusinessapp/src/components/About.js
+++ healthbusinessapp/src/components/About.js
@@ -13,6 +13,31 @@ const About = () => {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     e quisquam minus laborum asperiores, est obcaecati reiciendis</p>
             </div>
+            <div className="about-people-container">
+                <h3>What People Are Saying</h3>
+                <div className="about-people">
+                    <p>" Jessica taught me so much about a subject 
+                        I thought I was already an expert on: myself! "</p>
+                    <p>- KATY F.</p>
+                    <p className="test">" I was able to rekindle certain relationships
+                        with others from Jessica’s teachings. "</p>
+                    <p>- ELIZABETH B.</p>
+                </div>
+                {/* <div className="about-people people-quote">
+                    <p>" I was able to rekindle certain relationships
+                        with others from Jessica’s teachings. "</p>
+                    <p>- ELIZABETH B.</p>
+                </div> */}
+                <div className="about-people">
+                    <p>" My confidence in the workplace has gotten so much better! "</p>
+                    <p>- SARAH P.</p>
+                </div>
+                <div className="about-people">
+                    <p>" I let anxiety take over my life for too long. Now I have 
+                        the strength to control my emotions. "</p>
+                    <p>- VICTORIA G.</p>
+                </div>
+            </div>
             <Footer/>
         </div>
     )
