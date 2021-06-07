69f74a1601cd30ed89bae1f791d7d02557553b81
Melanie <melanienmedina@gmail.com>
2021-06-07 02:26:49 -0700

    need to optimize onscroll ani so the state will be followed properly

--- healthbusinessapp/src/components/Features.js
+++ healthbusinessapp/src/components/Features.js
@@ -1,44 +1,12 @@
-// import {isElementInViewport} from 'isElementInViewport';
-import { useEffect } from 'react';
+import { useState, useLayoutEffect, useRef } from 'react';
 
 const Features = ({ width }) => {
+	const [animate, setAnimate] = useState({itemOne: false, itemTwo: false});
 
-	function isElementInViewport(el) {
-		var rect = el.getBoundingClientRect();
-		return (
-		  (rect.top <= 0
-			&& rect.bottom >= 0)
-		  ||
-		  (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-			rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-		  ||
-		  (rect.top >= 0 &&
-			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-		);
-	}
-
-	useEffect(() => {
-		function onScrollAni(){
-			let scroll = window.requestAnimationFrame || 
-				function(callback){ window.setTimeout(callback, 1000/60)};
-			let elementsToShow = document.querySelectorAll('.show-on-scroll');
-
-			elementsToShow.forEach(function(el){
-			if(isElementInViewport(el)){
-				el.classList.add('is-visible');
-			} else{
-				el.classList.remove('is-visible');
-			}
-			});
-		
-			scroll(onScrollAni)
-			}
-			onScrollAni();
-	})
 
 	return (
 		<section className="features">
-			<div className="container features-container left-feature show-on-scroll">
+			<div className="features-container left-feature">
 				<div className="marketing-img">
 					<img
 						className="feature-img feature-women-img"
@@ -57,7 +25,7 @@ const Features = ({ width }) => {
 				</div>
 			</div>
 			{width <= 1000 ? (
-				<div className="container features-container show-on-scroll">
+				<div className="features-container right-feature">
 					<div className="next-features-img ">
 						<img
 							className="feature-img feature-group-img"
@@ -76,7 +44,7 @@ const Features = ({ width }) => {
 					</div>
 				</div>
 			) : (
-				<div className="container features-container right-feature show-on-scroll">
+				<div className="features-container right-feature">
 					<div className="left-col col">
 						<h1 className="self-care-msg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
 						<h4>
