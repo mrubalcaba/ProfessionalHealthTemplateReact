fbfcb08e6bb1f8465aeffaed80dea3bc00b1773d
Melanie <melanienmedina@gmail.com>
2021-06-12 20:54:47 -0700

    working on animations for home page

--- healthbusinessapp/src/components/Features.js
+++ healthbusinessapp/src/components/Features.js
@@ -2,11 +2,32 @@ import { useState, useLayoutEffect, useRef } from 'react';
 
 const Features = ({ width }) => {
 	const [animate, setAnimate] = useState({itemOne: false, itemTwo: false});
+	const refOne = useRef(null);
+	const refTwo = useRef(null);
 
+	useLayoutEffect(() => {
+		const topPos = element => element.getBoundingClientRect().top;
+		const divOnePos = topPos(refOne.current);
+		const divTwoPos = topPos(refTwo.current);
+
+		const onScroll = () => {
+			const scrollPos = window.scrollY + window.innerHeight;
+			if(divOnePos * 1.25 < scrollPos){
+				document.documentElement.style.setProperty('--position-one', '-80px');
+				setAnimate(state => ({...state, itemOne: true}));
+			}
+			else if(divTwoPos < scrollPos){
+				setAnimate(state => ({...state, itemTwo: true}));
+			}
+		}
+		
+		window.addEventListener("scroll", onScroll);
+		return () => window.removeEventListener("scroll", onScroll);
+	}, []);
 
 	return (
 		<section className="features">
-			<div className="features-container left-feature">
+			<div className="features-container left-feature" animate={animate.itemOne} ref={refOne}>
 				<div className="marketing-img">
 					<img
 						className="feature-img feature-women-img"
@@ -25,7 +46,7 @@ const Features = ({ width }) => {
 				</div>
 			</div>
 			{width <= 1000 ? (
-				<div className="features-container right-feature">
+				<div className="features-container right-feature" animate={animate.itemTwo} ref={refTwo}>
 					<div className="next-features-img ">
 						<img
 							className="feature-img feature-group-img"
@@ -44,7 +65,7 @@ const Features = ({ width }) => {
 					</div>
 				</div>
 			) : (
-				<div className="features-container right-feature">
+				<div className="features-container right-feature" animate={animate.itemTwo} ref={refTwo}>
 					<div className="left-col col">
 						<h1 className="self-care-msg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
 						<h4>
